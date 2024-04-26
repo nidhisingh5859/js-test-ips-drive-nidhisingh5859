2e4d516f5dd317ee2a15e9edb6f42a22d3ba9588
NIdhi Singh <133571695+nidhisingh5859@users.noreply.github.com>
2024-04-26 14:17:09 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,13 @@
-function longestSubstring(s) {
-
-
-    // Implementation of longestSubstring function
-}
-
-module.exports = { longestSubstring };
+class Solution:
+    def length_of_longest_substr(s: str) -> int:
+        result = 0
+        hash_map = {}
+        i = 0
+        j = 0
+        while j < len(s):
+            char = s[j]
+            if char in hash_map:
+                i = max(hash_map[char], i)
+            result = max(result, j - i + 1)
+            hash_map[char] = j + 1
+        return result
